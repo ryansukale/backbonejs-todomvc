f4da6374c0086c387615c1202942b31811861abb
Ryan Sukale <ryansukale@gmail.com>
2013-10-30 22:08:53 -0400

    Initial commit - just finished the scaffolding

---
+++ js/main.js
@@ -0,0 +1,13 @@
+require.config({
+	baseUrl : 'lib',
+    paths: {
+        foo: 'lib/'
+    }
+});
+
+require(["lib/jquery.min"], function(util) {
+    //This function is called when scripts/helper/util.js is loaded.
+    //If util.js calls define(), then this function is not fired until
+    //util's dependencies have loaded, and the util argument will hold
+    //the module value for "helper/util".
+});
